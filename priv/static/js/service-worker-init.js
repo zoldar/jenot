6c635bf97c4018dce61b2b5a2c3c24e7ff2a5a27
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-11-30 20:20:39 +0100

    Request permission to use persistent storage by service worker

--- priv/static/js/service-worker-init.js
+++ priv/static/js/service-worker-init.js
@@ -1,5 +1,19 @@
 const registerServiceWorker = async () => {
   if ("serviceWorker" in navigator) {
+    if (navigator.storage && navigator.storage.persist) {
+      navigator.storage.persist().then((persistent) => {
+        if (persistent) {
+          console.log(
+            "Storage will not be cleared except by explicit user action",
+          );
+        } else {
+          console.log(
+            "Storage may be cleared by the UA under storage pressure.",
+          );
+        }
+      });
+    }
+
     try {
       const registration = await navigator.serviceWorker.register(
         "/js/service-worker.js",
