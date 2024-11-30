97e5d9171d8884dfaa5100132f89d802bd736f34
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-12-01 00:44:32 +0100

    Fix terribly broken SW caching

--- priv/static/js/service-worker.js
+++ priv/static/js/service-worker.js
@@ -60,7 +60,8 @@ self.addEventListener("activate", (e) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.url.pathname.startsWith("/api")) {
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith("/api")) {
     return;
   }
 
